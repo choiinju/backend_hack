f147568e17484b1942b2be1b703b1ca952db8250
naye0n00 <wendy3302@naver.com>
2024-07-22 18:24:50 +0900

    초기 설정

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'yellow', 
+        'USER': 'root', 
+        'PASSWORD': '1234', 
+        'HOST': '127.0.0.1', 
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-b=yz1u88fye&!jsmysv=(vhsv!o1hve$l=u38%328870658xn%'
