9e3af8bc73c6d851adfd1546a1d807d5e051d10b
jaeyoung <pjyng115@naver.com>
2024-07-26 05:44:38 +0900

    Commit1

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+UserAdmin.fieldsets+=(("Custom fields", {"fields":("first_ex","card_number")}),)
+
+
+
--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    first_ex=models.CharField(max_length=10, unique=False, null=True)
+    card_number=models.CharField(max_length=20, unique=True,null=True)
+	
+    def __str__(self):
+        return self.email
+    
+
+#first_ex=우선권 보유 현황
+#card_number=카드 번호
