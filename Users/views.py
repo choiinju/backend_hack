fced0fb38842f10294989c84b4792f506176aaa0
jaeyoung <pjyng115@naver.com>
2024-07-28 09:35:27 +0900

    Jay-2

--- Users/views.py
+++ Users/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+# Users/views.py
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 
-def index(request):
-    return render(request, 'Users/index.html')
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
